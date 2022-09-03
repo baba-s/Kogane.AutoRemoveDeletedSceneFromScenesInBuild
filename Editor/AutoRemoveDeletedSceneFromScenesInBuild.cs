d97e48c978d42f4cccefd31c334e0252f1900897
baba-s <shotababa1206@gmail.com>
2022-09-03 11:21:25 +0900

    update

--- Editor/AutoRemoveDeletedSceneFromScenesInBuild.cs
+++ Editor/AutoRemoveDeletedSceneFromScenesInBuild.cs
@@ -1,14 +1,34 @@
 ﻿using System.IO;
 using System.Linq;
 using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
 
 namespace Kogane.Internal
 {
     /// <summary>
     /// Deleted なシーンを Build Settings の Scenes In Build から自動で削除するエディタ拡張
     /// </summary>
-    internal sealed class AutoRemoveDeletedSceneFromScenesInBuild : AssetPostprocessor
+    internal sealed class AutoRemoveDeletedSceneFromScenesInBuild
+        : AssetPostprocessor,
+          IPreprocessBuildWithReport
     {
+        //==============================================================================
+        // プロパティ
+        //==============================================================================
+        public int callbackOrder => 0;
+
+        //==============================================================================
+        // 関数
+        //==============================================================================
+        /// <summary>
+        /// ビルドを開始した時に呼び出されます
+        /// </summary>
+        void IPreprocessBuildWithReport.OnPreprocessBuild( BuildReport report )
+        {
+            Remove();
+        }
+
         //==============================================================================
         // 関数(static)
         //==============================================================================
@@ -25,10 +45,21 @@ namespace Kogane.Internal
         {
             const string path = "ProjectSettings/EditorBuildSettings.asset";
 
-            var isChangedScenes = ArrayUtility.Contains( importedAssets, path );
+            var isChangedScenes =
+                    ArrayUtility.Contains( importedAssets, path ) ||
+                    deletedAssets.Any( x => x.EndsWith( ".unity" ) )
+                ;
 
             if ( !isChangedScenes ) return;
 
+            Remove();
+        }
+
+        /// <summary>
+        /// Deleted なシーンを削除します
+        /// </summary>
+        private static void Remove()
+        {
             // Build Settings では Deleted なシーンかどうかは File.Exists で確認している
             // https://github.com/Unity-Technologies/UnityCsReference/blob/master/Editor/Mono/BuildPlayerSceneTreeView.cs
             var newScenes = EditorBuildSettings.scenes
--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 ![icon457](https://user-images.githubusercontent.com/6134875/187419014-c6d7a698-0258-4c9a-915a-33a6a0fca8fe.gif)
 
-プロジェクトを保存した時に Build Settings の Scenes In Build から Deleted なシーンを自動で削除するエディタ拡張
+Build Settings の Scenes In Build から Deleted なシーンを自動で削除するエディタ拡張
+
+## 使用例
+
+以下のタイミングで Build Settings の Scenes In Build から Deleted なシーンを自動で削除します
+
+* シーンファイル（.unity）を削除した時
+* プロジェクトを保存した時
+* ビルドを開始した時
